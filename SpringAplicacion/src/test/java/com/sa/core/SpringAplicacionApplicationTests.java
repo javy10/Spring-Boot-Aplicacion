c1eaef5d8f96a08f96b5b3b9d830cdd0bb6b9322
javy10 <javiierb6@gmail.com>
2020-01-20 15:06:21 -0600

    Commit inicial con spring. Close #1

---
+++ SpringAplicacion/src/test/java/com/sa/core/SpringAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
